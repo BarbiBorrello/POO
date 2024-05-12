37d11d5a3520ffb7478170a57d41f1f4001e8fb4
Barbi Borrello <biborrello@hotmail.com>
2024-05-12 20:29:49 -0300

    .

--- herencia/Producto.ts
+++ herencia/Producto.ts
@@ -4,6 +4,37 @@ class Producto {
   constructor(price:number, name :string){
     this.price=price;
     this.name=name;
+  }
 
+  getprice(){
+    return this.price;
   }
 }
+
+class ProductoElectronico extends Producto {
+
+   constructor (price:number,name:string,useBatteries:boolean){ // se agrega 1 parametro : useBatteries
+    super(price,name); // se llama al CONSTRUCTOR PADRE
+    this.useBatteries = useBatteries // se agrega si USA O NO USA baterias
+
+  }
+  useBatteries: boolean =false;
+
+  setBatteries(usa: boolean) {
+    this.useBatteries = usa;
+  }
+}
+
+// class ProductosSamsung extends ProductoElectronico {}
+
+function main () {
+  const celular = new ProductoElectronico(1000,"Samsung Galaxy",true);
+  // celular.setBatteries(true); => antes de EXTENDER la clase
+  console.log(celular);
+  
+
+
+  
+}
+
+main();
